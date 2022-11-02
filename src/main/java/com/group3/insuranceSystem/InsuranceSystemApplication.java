94bd4574792b2fe5881fd478ba37d79f022f07ea
amity <amity3744039@gmail.com>
2022-11-02 18:31:27 +0800

    feat: init project

---
+++ src/main/java/com/group3/insuranceSystem/InsuranceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.group3.insuranceSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceSystemApplication.class, args);
+	}
+
+}
