04beaaf053576386c299f077db0e6becf7278542
Wei Liu (RD-TW) <wei_liu@trendmicro.com>
2022-12-04 11:44:49 +0800

    add cros allow

--- src/main/java/com/group3/insuranceSystem/controller/PolicyController.java
+++ src/main/java/com/group3/insuranceSystem/controller/PolicyController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.group3.insuranceSystem.dao.PolicyAddress;
 import com.group3.insuranceSystem.dto.Policy;
 import com.group3.insuranceSystem.dto.Response;
@@ -15,6 +16,7 @@ import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PolicyController {
 	@Autowired
 	private PolicyRepository policyRepository;
